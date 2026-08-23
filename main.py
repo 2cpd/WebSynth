342bc15cbc689c1f0ee94df60bfbc115c9e87021
2cpd <lhy3408@gmail.com>
2026-08-23 18:53:54 +0800

    main: oscillator test -> its own method, called by main

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ from engine import AudioEngine
 SAMPLE_RATE = 44100
 BLOCK_SIZE = 512
 
-if __name__ == '__main__':
-    # [test all 4 types of oscillators]
+def test_oscillators():
+    """
+    test all oscillators: sine, saw, triangle, square @ 50% and 80%
+    (also tests noise for now)
+    """
     test_oscs = [
         oscillator.SineOscillator(freq=880, sample_rate=SAMPLE_RATE),
         oscillator.SawOscillator(freq=110, sample_rate=SAMPLE_RATE),
         oscillator.TriangleOscillator(freq=440, sample_rate=SAMPLE_RATE),
         oscillator.SquareOscillator(freq=220, sample_rate=SAMPLE_RATE, duty_cycle=0.5),
+        oscillator.SquareOscillator(freq=220, sample_rate=SAMPLE_RATE, duty_cycle=0.8),
     ]
 
     engine = AudioEngine(test_oscs[0], sample_rate=SAMPLE_RATE, block_size=BLOCK_SIZE)
     engine.start()
+    optional_param = ""
 
     for index, osc in enumerate(test_oscs):
-        print(f"Playing wave #{index}, frequency = {osc.freq} Hz, using {type(osc).__name__}")
+        if type(osc).__name__ == "SquareOscillator":
+            optional_param = ", duty cycle = " + str(osc.duty_cycle)
+
+        print(f"Playing wave #{index}, frequency = {osc.freq} Hz, using {type(osc).__name__}" + optional_param)
         # alternatively str(type(osc))[8:-2] instead of type(osc).__name__
 
         engine.osc = osc  # swap the oscillator
@@ -30,3 +38,6 @@ if __name__ == '__main__':
     time.sleep(2)
 
     engine.stop()
+
+if __name__ == '__main__':
+    test_oscillators()

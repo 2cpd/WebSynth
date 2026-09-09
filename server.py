2ba7607f2f66add59f63e604acb2322e36b75f77
2cpd <lhy3408@gmail.com>
2026-09-09 20:56:46 +0800

    updated web functionality to include on the fly ADSR and frequency adjustments (TODO: bugged osc selection, does not change with radio selection)

--- server.py
+++ server.py
@@ -2,7 +2,6 @@
 # serve html page
 # accept ws connections
 # parse json msgs
-import fastapi
 
 from envelope import Envelope
 import oscillator
@@ -33,11 +32,39 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_json()
-        if data.get("type") == "gate":
-            engine.gate_on = data.get("value", False)
+        msg_type = data.get("type")
+        msg_value = data.get("value")
+
+        if msg_type == "gate":
+            engine.gate_on = msg_value
+        elif msg_type == "freq":
+            engine.voice.osc.freq = float(msg_value)
+        elif msg_type == "waveform":
+            # change osc but preserve curr freq & phase
+            curr_freq = engine.voice.osc.freq
+            curr_phase = engine.voice.osc.phase
+
+            # wave types: sin, saw, tri, sqr
+                # no noise for now
+                # sqr needs load % (default to 50 for now)
+            if msg_value == "sin":
+                engine.voice.osc = oscillator.SineOscillator(curr_freq, engine.sample_rate)
+            elif msg_value == "saw":
+                engine.voice.osc = oscillator.SawOscillator(curr_freq, engine.sample_rate)
+            elif msg_value == "tri":
+                engine.voice.osc = oscillator.TriangleOscillator(curr_freq, engine.sample_rate)
+            elif msg_value == "sqr":
+                engine.voice.osc = oscillator.SquareOscillator(curr_freq, engine.sample_rate, 50)
+            engine.voice.osc.phase = curr_phase
+        elif msg_type == "attack":
+            engine.voice.env.set_attack(float(msg_value))
+        elif msg_type == "decay":
+            engine.voice.env.set_decay(float(msg_value))
+        elif msg_type == "sustain":
+            engine.voice.env.set_sustain(float(msg_value))
+        elif msg_type == "release":
+            engine.voice.env.set_release(float(msg_value))
 
 @app.on_event("shutdown")
 async def shutdown_event():
     engine.stop()
-
-
